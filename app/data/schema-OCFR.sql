b1e3a59e0e099542eb0beed3459765c3c198a90c
ipmcgrat <49279110+ipmcgrat@users.noreply.github.com>
2019-10-22 18:27:22 -0400

    sql

--- app/data/schema-OCFR.sql
+++ app/data/schema-OCFR.sql
@@ -1,29 +1,29 @@
 USE ocfr26;
 
 CREATE TABLE Staff (
-  name VARCHAR(64) PRIMARY KEY,
-  radionumber VARCHAR(64),
+  radionumber VARCHAR(64) PRIMARY KEY,
+  firstname VARCHAR(64),
+  lastname VARCHAR(64),
   station VARCHAR(64),
-  certification VARCHAR(64),
-  expirationdate DATE DEFAULT NULL
+  phone VARCHAR(64)
 );
 
-INSERT INTO Staff (name, radionumber, station, certification, expirationdate) VALUES
-("Taylor McManus", "1213", "North 2", "Firefighter 1", "2021-10-03"),
-("Ian McGrath", "1133", "West 3", "Fire Fighter 2", "2021-11-03"),
-("Morgan Morrow", "1234", "East 2", "Fire Fighter 1", "2021-12-03"),
-("Vishwesh Lamar", "1344", "North 1", "CPR Level 2", "2023-12-02");
+INSERT INTO Staff (radionumber, firstname, lastname, station, phone) VALUES
+("1213","Taylor", "McManus",  "North 2", "111-222-3333"),
+("1133","Ian", "McGrath" "West 3", "444-555-6666"),
+("1234","Morgan", "Morrow",  "East 2", "777-888-9999"),
+("1344","Vishwesh", "Lamar",  "North 1", "111-123-1234");
 
 CREATE TABLE Enroll(
-  name VARCHAR(64),
+  radionumber VARCHAR(64),
   certid INTEGER,
-  PRIMARY KEY (name,certid)
+  PRIMARY KEY (radionumber,certid)
 );
 
 INSERT INTO Enroll (name, certid) VALUES
-("Taylor McManus", 1),
-("Taylor McManus", 3),
-("Ian McGrath", 4);
+("1213", 1),
+("1213", 3),
+("1133", 4);
 
 CREATE TABLE Certifications (
   certid INTEGER PRIMARY KEY,
