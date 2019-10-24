4e41c484b6924acc0cb2817aef71c795320e972c
ipmcgrat <49279110+ipmcgrat@users.noreply.github.com>
2019-10-24 18:28:57 -0400

    cert form

--- app/public/api/records/certpost.php
+++ app/public/api/records/certpost.php
@@ -8,12 +8,11 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 $stmt = $db->prepare(
   'INSERT INTO Certifications
-    (certid, certname, agency, expyears)
+    (certname, agency, expyears)
   VALUES (?, ?, ?, ?)'
 );
 
 $stmt->execute([
-  $_POST['certid'],
   $_POST['certname'],
   $_POST['agency'],
   $_POST['expyears']
