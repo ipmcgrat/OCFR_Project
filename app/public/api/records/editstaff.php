ffe14514ffe6d7e620bb12b998044436582ba6f7
Morgan Morrow <momorrow@indiana.edu>
2019-10-25 16:40:26 -0400

    Update query

---
+++ app/public/api/records/editstaff.php
@@ -0,0 +1,9 @@
+<?php
+
+// Step 0: Validation
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+
+UPDATE Staff SET radionumber =?, lastname = "?", );
