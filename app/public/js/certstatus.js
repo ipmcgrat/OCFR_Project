34252857ee6a7b08647099f08e26ac619d858f46
Morgan Morrow <momorrow@indiana.edu>
2019-10-26 16:30:24 -0400

    cert status

---
+++ app/public/js/certstatus.js
@@ -0,0 +1,36 @@
+var certStatusApp = new Vue({
+  el: '#certStatusApp',
+  data: {
+    details: [],
+      recordDetails: {},
+
+      }
+
+  },
+  methods: {
+    fetchDetails() {
+      fetch('api/records/certstatus.php')
+      .then(response => response.json())
+      .then(json => { certStatusApp.details = json })
+
+this.handleReset();
+    },
+
+    handleReset() {
+      this.recordDetails = {
+        radionumber: '',
+        firstname: '',
+        lastname: '',
+        certname:'',
+        status:''
+      }
+
+    },
+
+},
+
+  created() {
+    this.handleReset();
+    this.fetchDetails();
+  }
+});
